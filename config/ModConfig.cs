c85690b6446328cc1ffb92826f82b2c9a619ea28
schmiedleander-sketch <schmiedleander@gmail.com>
2026-02-21 17:36:26 +0100

    update #3 config
    here you can config the mod

---
+++ config/ModConfig.cs
@@ -0,0 +1,8 @@
+public static class ModConfig
+{
+    public static float shrinkInterval = 30f;
+    public static float shrinkPercent = 0.1f;
+    public static float stompRange = 1.5f;
+    public static float stompCooldown = 15f;
+    public static float smallestThreshold = 0.3f;
+}
